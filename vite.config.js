498b7bfab447cc9f437e0fdbd3b653ae2fb65529
ThisIsInga <thisisingauwu@gmail.com>
2026-08-15 17:12:34 +1000

    изменения

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/aluminum_structures_static/',
   server: {
     port: 3002,
     host: true,
